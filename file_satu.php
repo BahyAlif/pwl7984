47e73ed6ab86ad558c86370c1b1d50ae5a98c725
BahyAlif <muhamad7984@students.amikom.ac.id>
2016-11-19 14:21:12 +0700

    Update file_satu.php

--- file_satu.php
+++ file_satu.php
@@ -1,8 +1,15 @@
-<HTML>
-<HEAD>
-<title></title>
-</HEAD>
-<body>
-
-</body>
+<html>
+  <head>
+    <title>For Loops</title>
+  </head>
+  <body>
+    <p>
+      <?php
+        // Echoes the first five even numbers
+        for ($i = 2; $i < 11; $i = $i + 2) {
+          echo $i;
+        }
+      ?>
+    </p>
+  </body>
 </html>
